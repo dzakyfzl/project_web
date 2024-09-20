1a52aaece392a8bb9792b4a816e1ca22eec379b4
Dzaky Fazli <dzakyfazli.11@gmail.com>
2024-09-20 18:21:33 +0700

    add .gitignore

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express') //Express
 const path = require('path')
 const app = express()
-const port = '3000'
+const port = '3000'//Port Localhost
 
 //Menyediakan semua image pada folder src ke online
 app.use('/src',express.static(path.join(__dirname, 'src')))
@@ -11,7 +11,7 @@ app.get('/',(req,res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-//Memulai Server untuk merender HTML
+//Memulai Server
 app.listen(port,()=>{
     console.log(`server start in http://localhost:${port}`)
 })
